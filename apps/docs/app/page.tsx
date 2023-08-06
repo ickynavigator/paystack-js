7c96b7cff390bdcd50439faaf9a3ab2032285f92
Obi Fortune <gabrielobi.of@gmail.com>
2023-08-06 18:19:16 +0100

    remove unused packages

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,10 +1,3 @@
-import { Button, Header } from "ui";
-
 export default function Page() {
-  return (
-    <>
-      <Header text="Docs" />
-      <Button />
-    </>
-  );
+  return <>Hello World</>;
 }
