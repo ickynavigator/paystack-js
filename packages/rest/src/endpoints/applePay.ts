88591e6fde776ae5db6d99bac8fa92a27b6cdf9a
Obi Fortune <gabrielobi.of@gmail.com>
2023-08-14 23:56:58 +0100

    corrected the query type

--- packages/rest/src/endpoints/applePay.ts
+++ packages/rest/src/endpoints/applePay.ts
@@ -44,7 +44,7 @@ export type Members = EndpointMemberParameters<
       body: {};
       query: {
         /** Flag to enable cursor pagination on the endpoint */
-        use_cursor: boolean;
+        use_cursor?: boolean;
         /** A cursor that indicates your place in the list. It can be used to fetch the next page of the list */
         next?: string;
         /** A cursor that indicates your place in the list. It should be used to fetch the previous page of the list after an intial next request */
