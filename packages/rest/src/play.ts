332b05954a106d62ece28331ab521114bc59ae01
Obi Fortune <gabrielobi.of@gmail.com>
2023-08-15 15:00:30 +0100

    mehhhh

--- packages/rest/src/play.ts
+++ packages/rest/src/play.ts
@@ -2,4 +2,10 @@ import Paystack from '.';
 
 const p = new Paystack('sk_test_1234567890');
 
-p.applePay.listDomains({});
+p.applePay.listDomains({
+  query: {
+    next: 'sttt',
+  },
+});
+
+p.miscellaneous.listBanks({});
